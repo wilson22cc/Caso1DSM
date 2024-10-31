97dae3dd2737aa0b5e90f0037cc2023087a06a6c
wilson22cc <131506264+wilson22cc@users.noreply.github.com>
2024-10-31 18:29:40 -0500

    Commit_31/10

--- src/Main.kt
+++ src/Main.kt
@@ -1,35 +1,57 @@
-open class Persona(
-    val nombre: String,
-    val apellidos: String,
-    val fechaNacimiento: String,
-    val sexo: String
-)
+import java.lang.reflect.Constructor
 
-class Empleado(
+open class Persona(
     nombre: String,
     apellidos: String,
     fechaNacimiento: String,
-    sexo: String,
-    val sueldoBruto: Int
-) :Persona(nombre, apellidos, fechaNacimiento, sexo){
+    sexo: Gender
+)
+{
+    var nombre: String = "-"
+    var apellidos: String = "-"
+    var fechaNacimiento: String = "ND"
+    var sexo: Gender = Gender.ND
+
+    init {
+        this.nombre = nombre
+        this.apellidos = apellidos
+        this.fechaNacimiento = fechaNacimiento
+        this.sexo = sexo
+    }
+}
+
+class Empleado:Persona{
+    var sueldoBruto: Int = 0
     val subordinados: MutableList<Empleado> = mutableListOf()
+
+    constructor(nombre: String,apellidos: String,fechaNacimiento: String,sexo: Gender)
+            : super(nombre,apellidos,fechaNacimiento,sexo)
+    constructor(nombre: String,apellidos: String,fechaNacimiento: String,sexo: Gender,sueldoBruto: Int)
+            : super(nombre, apellidos, fechaNacimiento,sexo) {
+                this.sueldoBruto = sueldoBruto
+        }
 
     fun anadirSubordinado(subordinado: Empleado){
         subordinados.add(subordinado)
     }
 }
 
-class Cliente(
-    nombre: String,
-    apellidos: String,
-    fechaNacimiento: String,
-    sexo: String,
-    val telefono: Int
-): Persona(nombre, apellidos, fechaNacimiento, sexo)
+class Cliente:Persona{
+    var telefono: String = "000000000"
+
+    constructor(nombre: String,apellidos: String,fechaNacimiento: String,sexo: Gender)
+            : super(nombre,apellidos,fechaNacimiento,sexo)
+    constructor(nombre: String,apellidos: String,fechaNacimiento: String,sexo: Gender,telefono: String)
+            : super(nombre,apellidos,fechaNacimiento,sexo){
+                this.telefono = telefono
+            }
+}
 
 class Empresa(
-    val nombre: String
-){
+    nombre: String
+)
+{
+    var nombre: String = "-"
     val empleados: MutableList<Empleado> = mutableListOf()
     val clientes: MutableList<Cliente> = mutableListOf()
 
@@ -59,14 +81,18 @@ class Empresa(
     }
 }
 
-fun main() {
-    val empleado1 = Empleado("Jhosselin","Clemente","","F",4000)
-    val empleado2 = Empleado("Joaquin","Del Aguila","","M",3500)
-    val empleado3 = Empleado("Jesus","Vasquez","400 aC","M",1200)
+enum class Gender {
+    MASCULINO,FEMENINO,ND
+}
 
-    val cliente1 = Cliente("Wilson","Leyva","2003","M",938266816)
-    val cliente2 = Cliente("Fiorella","Perez","","F",987654321)
-    val cliente3 = Cliente("Ricardo","Chuquivilca","","M",963852741)
+fun main() {
+    val empleado1 = Empleado("Jhosselin","Clemente","",Gender.FEMENINO,4000)
+    val empleado2 = Empleado("Joaquin","Del Aguila","",Gender.MASCULINO,3500)
+    val empleado3 = Empleado("Jesus","Vasquez","400 aC",Gender.MASCULINO,1200)
+
+    val cliente1 = Cliente("Wilson","Leyva","2003",Gender.MASCULINO,"938266816")
+    val cliente2 = Cliente("Fiorella","Perez","",Gender.FEMENINO,"987654321")
+    val cliente3 = Cliente("Ricardo","Chuquivilca","",Gender.MASCULINO,"963852741")
 
     val empresa = Empresa("Tech Solutions")
 
